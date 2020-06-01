aa99b13f2d8f2a06b35cb133216291cc2267e311
Dimitris Vogiatzidakis <dvogiatz@ics.forth.gr>
2020-06-01 03:15:56 +0300

    DataSet<Node> Vertices added

--- flinkmancer/src/main/java/flinkmancer/Flinkmancer.java
+++ flinkmancer/src/main/java/flinkmancer/Flinkmancer.java
@@ -5,12 +5,16 @@
  */
 package flinkmancer;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+import org.apache.flink.api.common.functions.FlatJoinFunction;
 import org.apache.flink.api.common.functions.GroupReduceFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple4;
+import org.apache.flink.api.java.tuple.Tuple8;
 import org.apache.flink.util.Collector;
 
 /**
@@ -25,7 +29,7 @@ public class Flinkmancer {
     public static void main(String[] args) throws Exception {
         //BasicConfigurator.configure(); uncomment gia log4j
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
-
+        env.setParallelism(4); // xwris den trexei logo heap size p moirazontai ta task. dn exw to flink-conf.yaml
 
         String followPath = "src/data/followers/";
         String replyPath = "src/data/reply/";
@@ -41,9 +45,12 @@ public class Flinkmancer {
         DataSet<Tuple2<Long, Set<Long>>> inc_follow = followSet.groupBy(0).reduceGroup(new GroupReduceFirst());
         DataSet<Tuple2<Long, Set<Long>>> out_follow = followSet.groupBy(1).reduceGroup(new GroupReduceSecond());
         System.out.println("Incoming follow set");
-        inc_follow.print();
+        // inc_follow.print();
         System.out.println("Outgoing follow set");
-        out_follow.print();
+        // out_follow.print();
+        System.out.println("All follow set");
+        DataSet<Tuple2<Long, Tuple2<Set<Long>, Set<Long>>>> all_follow = inc_follow.fullOuterJoin(out_follow).where("f0").equalTo("f0").with(new OuterJoin());
+        // all_follow.print();
 
 
         //Read reply set
@@ -58,6 +65,9 @@ public class Flinkmancer {
         //inc_reply.print();
         System.out.println("Outgoing reply set");
         //out_reply.print();
+        System.out.println("All reply set");
+        DataSet<Tuple2<Long, Tuple2<Set<Long>, Set<Long>>>> all_reply = inc_reply.fullOuterJoin(out_reply).where("f0").equalTo("f0").with(new OuterJoin());
+        //all_reply.print();
 
         //Read retweet set
         DataSet<Tuple2<Long, Long>> retweetSet = env.readCsvFile(retweetPath)
@@ -71,6 +81,9 @@ public class Flinkmancer {
         //inc_retweet.print();
         System.out.println("Outgoing retweet set");
         //out_retweet.print();
+        System.out.println("All retweet set");
+        DataSet<Tuple2<Long, Tuple2<Set<Long>, Set<Long>>>> all_retweet = inc_retweet.fullOuterJoin(out_retweet).where("f0").equalTo("f0").with(new OuterJoin());
+        //all_retweet.print();
 
         //Read quote set
         DataSet<Tuple2<Long, Long>> quoteSet = env.readCsvFile(quotePath)
@@ -84,7 +97,33 @@ public class Flinkmancer {
         //inc_quote.print();
         System.out.println("Outgoing quote set");
         //out_quote.print();
+        System.out.println("All quote set");
+        DataSet<Tuple2<Long, Tuple2<Set<Long>, Set<Long>>>> all_quote = inc_quote.fullOuterJoin(out_quote).where("f0").equalTo("f0").with(new OuterJoin());
+        //all_quote.print();
+        DataSet<Tuple2<Long, Tuple4<Set<Long>, Set<Long>, Set<Long>, Set<Long>>>> followXreply = all_follow.fullOuterJoin(all_reply).where("f0").equalTo("f0").with(new OuterJoinTuple4());
+        //followXreply.print();
 
+        DataSet<Tuple2<Long, Tuple4<Set<Long>, Set<Long>, Set<Long>, Set<Long>>>> retweetXquote = all_retweet.fullOuterJoin(all_quote).where("f0").equalTo("f0").with(new OuterJoinTuple4());
+        //retweetXquote.print();
+
+        DataSet<Tuple2<Long, Tuple8<Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>>>> all = followXreply.fullOuterJoin(retweetXquote).where("f0").equalTo("f0").with(new OuterJoinTuple8());
+        //all.print();
+        DataSet<Node> vertices = all.groupBy(0).reduceGroup(new NodeCreator());
+        // vertices.print();
+        //DataSet<Tuple2<Node, Node>> Vpairs = vertices.cross(vertices);  takes more than 15 mins. 
+        //Vpairs.print();
+
+        /*
+        DataSet<Tuple2<Long, Tuple2<Tuple2<Set<Long>, Set<Long>>, Tuple2<Set<Long>, Set<Long>>>>> followXreply = all_follow.fullOuterJoin(all_reply).where("f0").equalTo("f0").with(new OuterJoinTuple2());
+        //followXreply.print();
+        DataSet<Tuple2<Long, Tuple2<Tuple2<Set<Long>, Set<Long>>, Tuple2<Set<Long>, Set<Long>>>>> retweetXquote = all_retweet.fullOuterJoin(all_quote).where("f0").equalTo("f0").with(new OuterJoinTuple2());
+        //retweetXquote.print();
+
+        DataSet<Tuple2<Long, Tuple2<Tuple2<Tuple2<Set<Long>, Set<Long>>, Tuple2<Set<Long>, Set<Long>>>, Tuple2<Tuple2<Set<Long>, Set<Long>>, Tuple2<Set<Long>, Set<Long>>>>>> all = followXreply.fullOuterJoin(retweetXquote).where("f0").equalTo("f0").with(new OuterJoinTuple22());
+        System.out.println("All set");
+        // all.print();
+
+         */
     }
     /*
     * used in reduceGroup to create the list of neighbors
@@ -131,4 +170,194 @@ public class Flinkmancer {
         }
     }
 
+    public static class NodeCreator
+            implements GroupReduceFunction<Tuple2<Long, Tuple8<Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>>>, Node> {
+
+        @Override
+        public void reduce(Iterable<Tuple2<Long, Tuple8<Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>>>> in, Collector<Node> out) {
+
+            ArrayList<Set<Long>> ali = new ArrayList<>();
+            Long key = null;
+
+            // add all strings of the group to the set
+            for (Tuple2<Long, Tuple8<Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>>> t : in) {
+                key = t.f0;
+                ali.add(t.f1.f0);     //follow in - reply - retweet - quote
+                ali.add(t.f1.f1);     //follow out
+                ali.add(t.f1.f2);     //reply in
+                ali.add(t.f1.f3);     //reply out
+                ali.add(t.f1.f4);     //retweet in
+                ali.add(t.f1.f5);     //retweet out
+                ali.add(t.f1.f6);     //quote in
+                ali.add(t.f1.f7);     //quote out
+
+            }
+
+            out.collect(new Node(key, ali));
+
+        }
+    }
+
+
+    public static class OuterJoin
+            implements FlatJoinFunction<Tuple2<Long, Set<Long>>, Tuple2<Long, Set<Long>>, Tuple2<Long, Tuple2<Set<Long>, Set<Long>>>> {
+
+        @Override
+        public void join(Tuple2<Long, Set<Long>> dataset1, Tuple2<Long, Set<Long>> dataset2, Collector<Tuple2<Long, Tuple2<Set<Long>, Set<Long>>>> out) {
+            if (dataset1 != null && dataset2 != null) {
+                Long key = dataset1.f0;
+
+            Set<Long> s1 = dataset1.f1;
+            Set<Long> s2 = dataset2.f1;
+
+
+                out.collect(new Tuple2<>(key, (new Tuple2<>(s1, s2))));
+            } else if (dataset1 != null) {
+                Long key = dataset1.f0;
+                Set<Long> s1 = dataset1.f1;
+                out.collect(new Tuple2<>(key, (new Tuple2<>(s1, new HashSet<>()))));
+            } else if (dataset2 != null) {
+                Long key = dataset2.f0;
+                Set<Long> s2 = dataset2.f1;
+                out.collect(new Tuple2<>(key, (new Tuple2<>(new HashSet<>(), s2))));
+            }
+
+        }
+    }
+
+    public static class OuterJoinTuple4
+            implements FlatJoinFunction<Tuple2<Long, Tuple2<Set<Long>, Set<Long>>>, Tuple2<Long, Tuple2<Set<Long>, Set<Long>>>, Tuple2<Long, Tuple4<Set<Long>, Set<Long>, Set<Long>, Set<Long>>>> {
+
+        @Override
+        public void join(Tuple2<Long, Tuple2<Set<Long>, Set<Long>>> dataset1, Tuple2<Long, Tuple2<Set<Long>, Set<Long>>> dataset2, Collector<Tuple2<Long, Tuple4<Set<Long>, Set<Long>, Set<Long>, Set<Long>>>> out) {
+            Set<Long> s1 = new HashSet<>();
+            Set<Long> s2 = new HashSet<>();
+            Set<Long> s3 = new HashSet<>();
+            Set<Long> s4 = new HashSet<>();
+            Long key = null;
+
+            if (dataset1 != null) {
+                if (dataset1.f1 != null) {
+                    key = dataset1.f0;
+                    s1 = dataset1.f1.f0;
+                    s2 = dataset1.f1.f1;
+                }
+            }
+            if (dataset2 != null) {
+                if (dataset2.f1 != null) {
+                    key = dataset2.f0;
+                    s3 = dataset2.f1.f0;
+                    s4 = dataset2.f1.f1;
+                }
+            }
+            if (dataset1 != null || dataset2 != null) {
+                out.collect(new Tuple2<>(key, (new Tuple4<>(s1, s2, s3, s4))));
+            }
+
+        }
+    }
+
+    public static class OuterJoinTuple8
+            implements FlatJoinFunction<Tuple2<Long, Tuple4<Set<Long>, Set<Long>, Set<Long>, Set<Long>>>, Tuple2<Long, Tuple4<Set<Long>, Set<Long>, Set<Long>, Set<Long>>>, Tuple2<Long, Tuple8<Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>>>> {
+
+        @Override
+        public void join(Tuple2<Long, Tuple4<Set<Long>, Set<Long>, Set<Long>, Set<Long>>> dataset1, Tuple2<Long, Tuple4<Set<Long>, Set<Long>, Set<Long>, Set<Long>>> dataset2, Collector<Tuple2<Long, Tuple8<Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>, Set<Long>>>> out) {
+            Set<Long> s1 = new HashSet<>();
+            Set<Long> s2 = new HashSet<>();
+            Set<Long> s3 = new HashSet<>();
+            Set<Long> s4 = new HashSet<>();
+            Set<Long> s5 = new HashSet<>();
+            Set<Long> s6 = new HashSet<>();
+            Set<Long> s7 = new HashSet<>();
+            Set<Long> s8 = new HashSet<>();
+            Long key = null;
+
+            if (dataset1 != null) {
+                if (dataset1.f1 != null) {
+                    key = dataset1.f0;
+                    s1 = dataset1.f1.f0;
+                    s2 = dataset1.f1.f1;
+                    s3 = dataset1.f1.f2;
+                    s4 = dataset1.f1.f3;
+                }
+            }
+            if (dataset2 != null) {
+                if (dataset2.f1 != null) {
+                    key = dataset2.f0;
+                    s5 = dataset2.f1.f0;
+                    s6 = dataset2.f1.f1;
+                    s7 = dataset2.f1.f2;
+                    s8 = dataset2.f1.f3;
+                }
+            }
+            if (dataset1 != null || dataset2 != null) {
+                out.collect(new Tuple2<>(key, (new Tuple8<>(s1, s2, s3, s4, s5, s6, s7, s8))));
+            }
+
+        }
+    }
+    /*
+    
+    public static class OuterJoinTuple2
+            implements FlatJoinFunction<Tuple2<Long, Tuple2<Set<Long>, Set<Long>>>, Tuple2<Long, Tuple2<Set<Long>, Set<Long>>>, Tuple2<Long, Tuple2<Tuple2<Set<Long>, Set<Long>>,Tuple2<Set<Long>, Set<Long>>>>> {
+
+        @Override
+        public void join(Tuple2<Long, Tuple2<Set<Long>, Set<Long>>> dataset1, Tuple2<Long, Tuple2<Set<Long>, Set<Long>>> dataset2, Collector<Tuple2<Long, Tuple2<Tuple2<Set<Long>, Set<Long>>,Tuple2<Set<Long>, Set<Long>>>>> out) {
+            if (dataset1 != null && dataset2 != null) {
+                Long key = dataset1.f0;
+                Tuple2<Set<Long>,Set<Long>> s1 = dataset1.f1;
+                Tuple2<Set<Long>,Set<Long>> s2 = dataset2.f1;
+                out.collect(new Tuple2<>(key, (new Tuple2<>(s1, s2))));
+            } else if (dataset1 != null) {
+                Long key = dataset1.f0;
+                Tuple2<Set<Long>,Set<Long>> s1 = dataset1.f1;
+                Set<Long> t1 = new HashSet<>();
+                Set<Long> t2 = new HashSet<>();
+                Tuple2<Set<Long>,Set<Long>> t3 = new Tuple2<>(t1,t2);
+                out.collect(new Tuple2<>(key, (new Tuple2<>(s1, t3))));
+            } else if (dataset2 != null) {
+                Long key = dataset2.f0;
+                Tuple2<Set<Long>,Set<Long>> s2 = dataset2.f1;
+                Set<Long> t1 = new HashSet<>();
+                Set<Long> t2 = new HashSet<>();
+                Tuple2<Set<Long>,Set<Long>> t3 = new Tuple2<>(t1,t2);
+                out.collect(new Tuple2<>(key, (new Tuple2<>(t3, s2))));
+            }
+
+        }
+    }
+
+    public static class OuterJoinTuple22
+            implements FlatJoinFunction<Tuple2<Long, Tuple2<Tuple2<Set<Long>, Set<Long>>, Tuple2<Set<Long>, Set<Long>>>>, Tuple2<Long, Tuple2<Tuple2<Set<Long>, Set<Long>>, Tuple2<Set<Long>, Set<Long>>>>, Tuple2<Long, Tuple2<Tuple2<Tuple2<Set<Long>, Set<Long>>, Tuple2<Set<Long>, Set<Long>>>, Tuple2<Tuple2<Set<Long>, Set<Long>>, Tuple2<Set<Long>, Set<Long>>>>>> {
+
+        @Override
+        public void join(Tuple2<Long, Tuple2<Tuple2<Set<Long>, Set<Long>>, Tuple2<Set<Long>, Set<Long>>>> dataset1, Tuple2<Long, Tuple2<Tuple2<Set<Long>, Set<Long>>, Tuple2<Set<Long>, Set<Long>>>> dataset2, Collector<Tuple2<Long, Tuple2<Tuple2<Tuple2<Set<Long>, Set<Long>>, Tuple2<Set<Long>, Set<Long>>>, Tuple2<Tuple2<Set<Long>, Set<Long>>, Tuple2<Set<Long>, Set<Long>>>>>> out) {
+            if (dataset1 != null && dataset2 != null) {
+                Long key = dataset1.f0;
+                Tuple2<Tuple2<Set<Long>, Set<Long>>, Tuple2<Set<Long>, Set<Long>>> s1 = dataset1.f1;
+                Tuple2<Tuple2<Set<Long>, Set<Long>>, Tuple2<Set<Long>, Set<Long>>> s2 = dataset2.f1;
+                out.collect(new Tuple2<>(key, (new Tuple2<>(s1, s2))));
+            } else if (dataset1 != null) {
+                Long key = dataset1.f0;
+                Tuple2<Tuple2<Set<Long>, Set<Long>>, Tuple2<Set<Long>, Set<Long>>> s1 = dataset1.f1;
+                Set<Long> t1 = new HashSet<>();
+                Set<Long> t2 = new HashSet<>();
+                Tuple2<Set<Long>, Set<Long>> t3 = new Tuple2<>(t1, t2);
+                Tuple2<Set<Long>, Set<Long>> t4 = new Tuple2<>(t1, t2);
+                Tuple2<Tuple2<Set<Long>, Set<Long>>, Tuple2<Set<Long>, Set<Long>>> t5 = new Tuple2<>(t3, t4);
+                out.collect(new Tuple2<>(key, (new Tuple2<>(s1, t5))));
+            } else if (dataset2 != null) {
+                Long key = dataset2.f0;
+                Tuple2<Tuple2<Set<Long>, Set<Long>>, Tuple2<Set<Long>, Set<Long>>> s2 = dataset2.f1;
+                Set<Long> t1 = new HashSet<>();
+                Set<Long> t2 = new HashSet<>();
+                Tuple2<Set<Long>, Set<Long>> t3 = new Tuple2<>(t1, t2);
+                Tuple2<Set<Long>, Set<Long>> t4 = new Tuple2<>(t1, t2);
+                Tuple2<Tuple2<Set<Long>, Set<Long>>, Tuple2<Set<Long>, Set<Long>>> t5 = new Tuple2<>(t3, t4);
+                out.collect(new Tuple2<>(key, (new Tuple2<>(t5, s2))));
+            }
+
+        }
+    }
+    */
 }
---
+++ flinkmancer/src/main/java/flinkmancer/Node.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flinkmancer;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+/**
+ *
+ * @author Hergal
+ */
+public class Node {
+    private Long id;
+    private ArrayList<Set<Long>> sets;
+
+    //Constructors
+    Node() {
+        this.id = null;
+        this.sets = new ArrayList<>();
+    }
+
+    Node(Long id) {
+        this.id = id;
+        this.sets = new ArrayList<>();
+    }
+
+    Node(Long id, ArrayList<Set<Long>> sets) {
+        this.id = id;
+        this.sets = sets;
+    }
+
+    //Setters & Getters
+    public ArrayList<Set<Long>> getSets() {
+        return this.sets;
+    }
+
+    public void setSets(ArrayList<Set<Long>> sets) {
+        this.sets = sets;
+    }
+
+    public Long getID() {
+        return this.id;
+    }
+
+    public void setID(Long id) {
+        this.id = id;
+    }
+    //Specific Sets
+    //Sets go from 0-7. even inc. odd out. follow - reply - retweet - quote
+
+    public void addSet(Set<Long> s) {
+        this.sets.add(s);
+    }
+
+    public Set<Long> getSet(int i) {
+        return this.sets.get(i);
+    }
+
+    public ArrayList<Long> getArrayList(int i) {
+        ArrayList<Long> al = new ArrayList<>();
+        al.addAll(sets.get(i));
+        return al;
+    }
+    @Override
+    public String toString() {
+        return "vertex id: " + this.getID() + " yey ";
+    }
+}
